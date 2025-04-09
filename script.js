cf3ec493ef7dd4d22d091140878ee9bfc4341f7e
vsjbrhvbebohdandov <bohdandovbenko1@gmail.com>
2025-04-09 17:31:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('scanner').addEventListener('mousedown', function () {
+document.getElementById('high-power-button').addEventListener('click', function () {
     const scanner = document.getElementById('scanner');
     const scanText = document.getElementById('scan-text');
     const result = document.getElementById('result');
@@ -12,8 +12,28 @@ document.getElementById('scanner').addEventListener('mousedown', function () {
         scanner.classList.remove('scanning');
         scanText.textContent = 'Поставте палець...';
 
-        // Генерація випадкової потужності (від 50 до 100)
+        // Генерація потужності більше 50%
         const power = Math.floor(Math.random() * 51) + 50;
         result.textContent = `Ваша потужність: ${power}%`;
-    }, 3000); // Затримка 3 секунди
+    }, 3000);
+});
+
+document.getElementById('low-power-button').addEventListener('click', function () {
+    const scanner = document.getElementById('scanner');
+    const scanText = document.getElementById('scan-text');
+    const result = document.getElementById('result');
+
+    // Початок сканування
+    scanner.classList.add('scanning');
+    scanText.textContent = 'Сканування...';
+
+    // Затримка для імітації процесу сканування
+    setTimeout(() => {
+        scanner.classList.remove('scanning');
+        scanText.textContent = 'Поставте палець...';
+
+        // Генерація потужності менше 35%
+        const power = Math.floor(Math.random() * 35);
+        result.textContent = `Ваша потужність: ${power}%`;
+    }, 3000);
 });
